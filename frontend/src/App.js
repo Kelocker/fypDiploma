947fb860a53fa20c3eed93f7807a2833f9c7dc17
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-03-20 22:21:26 +0800

    Integrating Django and React with axios

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Create from './components/Create';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/create" element={<Create />} />
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
