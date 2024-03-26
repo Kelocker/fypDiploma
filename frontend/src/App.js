4f6aa7cdfc5d8c2cb5f1f4c72026b89cae78cb16
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-03-27 00:54:52 +0800

    react-dom-setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,9 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Create from './components/Create';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/create" element={<Create />} />
-
-      </Routes>
-      
-    </div>
+    <Outlet />
   );
 }
 
