fef72abaaba55054f9dde61a5a89a65baf0a7f16
Hui Wen <chuaa41@gmail.com>
2024-03-27 10:44:05 +0800

    userProfile updated

--- frontend/src/user/userProfile.jsx
+++ frontend/src/user/userProfile.jsx
@@ -7,49 +7,54 @@ const userProfile = () => {
    
     <div className="Setting">
       <h1>Settings</h1>
-      
       <div className="User">
-        <div class="avatar-container">
+        <div className="avatar-container">
           <img src="{userAvatarUrl}" alt="" className='avatar-container'/>
-          <div class="camera-icon"></div>
+          <div className="camera-icon"></div>
         </div>
         <div className='username'>
-        Username:
+          Username:
         </div>
       </div>
       <div className='formContainer'>
         <table>
-          <h2>Basic Info</h2>
-          <tr>
-            <td>First Name</td>
-            <td><input type="text" name="firstName" /></td>
-            <td>Edit</td>
-          </tr>
-          <tr>
-            <td>Last Name</td>
-            <td><input type="text" name="lastName" /></td>
-            <td>Edit</td>
-          </tr>
-          <tr>
-            <td>Gender</td>
-            <td>
-              <input type="radio" id="male" name="gender" value="male"/>
-              <label for="male">Male</label>
-              <input type="radio" id="female" name="gender" value="female"/>
-              <label for="female">Female</label>
-            </td>
-            <td>Edit</td>
-          </tr>
-          <tr>
-            <td>Email</td>
-            <td><input type="email" name="email" /></td>
-            <td>Edit</td>
-          </tr>
-          <tr>
-            <td>Password</td>
-            <td><input type="password" name="password" /></td>
-            <td>Edit</td>
-          </tr>
+          <thead>
+            <tr>
+              <th><h2>Basic Info</h2></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>First Name</td>
+              <td><input type="text" name="firstName" /></td>
+              <td>Edit</td>
+            </tr>
+            <tr>
+              <td>Last Name</td>
+              <td><input type="text" name="lastName" /></td>
+              <td>Edit</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>
+                <input type="radio" id="male" name="gender" value="male"/>
+                <label htmlFor="male">Male</label>
+                <input type="radio" id="female" name="gender" value="female"/>
+                <label htmlFor="female">Female</label>
+              </td>
+              <td>Edit</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td><input type="email" name="email" autoComplete="email"/></td>
+              <td>Edit</td>
+            </tr>
+            <tr>
+              <td>Password</td>
+              <td><input type="password" name="password" /></td>
+              <td>Edit</td>
+            </tr>
+          </tbody>
         </table>
       </div>  
     </div>
