63671515810ce4fd2a0d1a34593ce6bf658d1452
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-04-04 21:40:59 +0800

    dashboard

---
+++ frontend/src/screens/dashboard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/dashboard/dashboard.css'
+import DashboardNavbar from '../components/dashboardNavbar'
+import DashboardSidebar from '../components/dashboardSidebar'
+
+const Dashboard = () => {
+  return (
+    <>
+      <DashboardNavbar />
+      <div className="DashboardWrapper">
+
+        <div className="DashboardSidebar">
+          <DashboardSidebar />
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default Dashboard
