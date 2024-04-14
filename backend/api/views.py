c2817632f6676d06de9dd5319c54e4ce0815e28c
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-04-14 16:36:46 +0800

    refactoring backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Hello, World!')
+class CreatreUserView(generics.CreateAPIView):
+
+    # specify the queryset on what object to be looking for
+    queryset = User.objects.all()
+
+    # what kind of data we need to accpet
+    serializer_class = UserSerializer
+
+    #  who can call this
+    permission_classes = [AllowAny]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for crud project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import CreatreUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path("api/user/register/", CreatreUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("api-auth/", include("rest_framework.urls")),
 ]
