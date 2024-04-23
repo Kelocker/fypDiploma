689e6d39620cd61b3c1d742c696249078322764c
LIM GEN JACK <135495371+Jack-1118@users.noreply.github.com>
2024-04-23 15:39:46 +0800

    sort by order

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,7 @@ from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.http import JsonResponse
 from .models import Exercise
+from django.db.models import Case, When, Value, IntegerField
 
 
 # Create your views here.
@@ -25,11 +26,19 @@ def exercise_list(request):
     # Get the difficulty level from the query parameters
     difficulty = request.GET.get('difficulty')
 
+    # Define the sort order for each difficulty level
+    difficulty_sort_order = Case(
+        When(difficulty_level='B', then=Value(1)),
+        When(difficulty_level='I', then=Value(2)),
+        When(difficulty_level='A', then=Value(3)),
+        output_field=IntegerField(),
+    )
+
     # Filter the queryset based on the difficulty if it's provided
     if difficulty:
         exercises = Exercise.objects.filter(difficulty_level=difficulty)
     else:
-        exercises = Exercise.objects.all()
+        exercises = Exercise.objects.all().annotate(sort_order=difficulty_sort_order).order_by('sort_order')
 
  
     exercise_list = [{
