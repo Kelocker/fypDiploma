b87bcb85be67c2e9fbedc25391a073243fcf0c49
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-06-07 20:58:51 +0800

    final for price components

--- frontend/src/components/Price.jsx
+++ frontend/src/components/Price.jsx
@@ -15,8 +15,8 @@ const Price = () => {
 
                 <div className="Plan-type">
 
-                    <span>
-                        {/* Most popular */}
+                    <span className="invisible-font">
+                        .
                     </span>
 
                     <span className="Plan-level">
@@ -62,8 +62,10 @@ const Price = () => {
 
                 <div className="Plan-type">
 
-                    <span>
-                        {/* Most popular */}
+                    <span className="most-popular">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7db5b6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+
+                        Most popular
                     </span>
 
                     <span className="Plan-level">
@@ -113,8 +115,8 @@ const Price = () => {
 
                 <div className="Plan-type">
 
-                    <span>
-                        {/* Most popular */}
+                    <span className="invisible-font">
+                        .
                     </span>
 
                     <span className="Plan-level">
@@ -137,7 +139,6 @@ const Price = () => {
                         <li>Learning</li>
                         <li>Excercise</li>
                         <li>Rank</li>
-                        <li>Compiler</li>
                     </ul>
                     
                     </span>
@@ -158,6 +159,7 @@ const Price = () => {
                 </div>
                 
 
+            
             </div>
         </div>
     </div>
